5dfa947ff86d1ff63e828223cdd7f11fca8bfaa8
heowan <101536766+heowan@users.noreply.github.com>
2023-08-31 16:33:33 +0900

    fix: 무한스크롤 무한로딩 문제 해결, intersection Observer 로직분리

--- src/pages/IssueListPage/index.tsx
+++ src/pages/IssueListPage/index.tsx
@@ -5,52 +5,31 @@ import LoadingSpinner from '../../components/loading/Loading';
 import IssueItem from '../../components/issueItem/IssueItem';
 import { useNavigate } from 'react-router-dom';
 import AdBanner from '../../components/AdBanner/AdBanner';
+import useIntersectionObserver from '../../utils/useIntersectionObserver';
 
 function IssueList() {
 	const [issues, setIssues] = useState<IssueListType[]>([]);
 	const [currentPage, setCurrentPage] = useState<number>(1);
-	const [isloading, setIsLoading] = useState<boolean>(true);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const navigate = useNavigate();
-
 	const observerRef = useRef<any>(null);
 
 	useEffect(() => {
-		getIssueListData();
-	}, []);
+		getIssueList(currentPage)
+			.then((issues: any) => {
+				setIssues(prevList => [...prevList, ...issues]);
+				setIsLoading(false);
+			})
+			.catch(error => {
+				navigate('/error');
+			});
+	}, [currentPage, navigate]);
 
-	const getIssueListData = async () => {
-		setIsLoading(true);
-		try {
-			const res = await getIssueList(currentPage);
-			// @ts-ignore
-			setIssues(prevIssues => [...prevIssues, ...res]);
-			setCurrentPage(prevPage => prevPage + 1);
-		} catch (err) {
-			console.error(err);
-		} finally {
-			setIsLoading(false);
-		}
-	};
+	useIntersectionObserver(observerRef, () => {
+		setCurrentPage(currentPage + 1);
+	});
 
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			entries => {
-				if (entries[0].isIntersecting) {
-					setCurrentPage(prev => prev + 1);
-				}
-			},
-			{ threshold: 1 },
-		);
-		if (observerRef.current) {
-			observer.observe(observerRef.current);
-		}
-
-		return () => {
-			observer.disconnect();
-		};
-	}, [issues]);
-
-	if (isloading) return <LoadingSpinner />;
+	if (isLoading) return <LoadingSpinner />;
 
 	return (
 		<>
